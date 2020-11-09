1336814e7ae20e352d122401e35505cbb3fd0b1b
KmgF <72263892+KmgF@users.noreply.github.com>
2020-11-09 21:34:20 +0900

    userモデル 本名のフォーマットへのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name ,presence: true
-  validates :family_name,presence: true
-  validates :first_name_kana,presence: true
-  validates :family_name_kana,presence: true
+  validates :first_name ,presence: true, format:{with:/\A[ぁ-んァ-ン一-龥]/,message:"漢字、ひらがな、全角カタカナのみ使用できます"}
+  validates :family_name,presence: true, format:{with:/\A[ぁ-んァ-ン一-龥]/,message:"漢字、ひらがな、全角カタカナのみ使用できます"}
+  validates :first_name_kana,presence: true,format:[with:/\A[ァ-ヶー－]+\z/,message:"全角カタカナのみ使用できます"]
+  validates :family_name_kana,presence: true,format:[with:/\A[ァ-ヶー－]+\z/,message:"全角カタカナのみ使用できます"]
   validates :birthday,presence: true
 end

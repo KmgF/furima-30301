b26d0f1712d6591ed581d7dc6e942e8978bf0204
KmgF <72263892+KmgF@users.noreply.github.com>
2020-11-23 00:01:57 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :password ,format:{with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/}
-  validates :first_name ,presence: true, format:{with:/\A[ぁ-んァ-ン一-龥]/,message:"漢字、ひらがな、全角カタカナのみ使用できます"}
-  validates :family_name,presence: true, format:{with:/\A[ぁ-んァ-ン一-龥]/,message:"漢字、ひらがな、全角カタカナのみ使用できます"}
-  validates :first_name_kana,presence: true,format:{with:/\A[ァ-ヶー－]+\z/,message:"全角カタカナのみ使用できます"}
-  validates :family_name_kana,presence: true,format:{with:/\A[ァ-ヶー－]+\z/,message:"全角カタカナのみ使用できます"}
-  validates :birthday,presence: true
-
-
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' }
+  validates :birthday, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
-    factory :user do
-      nickname                {Faker::Name.name}
-      email                   {Faker::Internet.free_email}
-      password                {Faker::Internet.password(min_length: 6)}
-      password_confirmation   {password}
-      first_name              {"安倍"}
-      family_name             {"晋三"}
-      first_name_kana         {"アベ"}
-      family_name_kana        {"シンゾウ"}
-      birthday                {Faker::Date.birthday(min_age: 0, max_age:100)}
-    end
+  factory :user do
+    nickname                { Faker::Name.name }
+    email                   { Faker::Internet.free_email }
+    password                { Faker::Internet.password(min_length: 6) }
+    password_confirmation   { password }
+    first_name              { '安倍' }
+    family_name             { '晋三' }
+    first_name_kana         { 'アベ' }
+    family_name_kana        { 'シンゾウ' }
+    birthday                { Faker::Date.birthday(min_age: 0, max_age: 100) }
+  end
 end

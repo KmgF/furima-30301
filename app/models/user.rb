27888adb44d5f0d5cae3940a8b03612f5b2e663e
KmgF <72263892+KmgF@users.noreply.github.com>
2020-11-24 00:38:12 +0900

    共通のバリデーションをwith_optionを用いて書き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,41 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' }
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' }
-  validates :birthday, presence: true
+         
+    with_options presence: true do
+      validates :nickname
+      validates :first_name
+      validates :family_name
+      validates :first_name_kana
+      validates :family_name_kana
+      validates :birthday
+    end
+    validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+    
+    # validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' }
+    # validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' }
+    # validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' }
+    # validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' }
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' } do
+      validates :first_name
+      validates :family_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' } do
+      validates :first_name_kana
+      validates :family_name_kana
+    end
+  
+    
+  
+  
+  # validates :nickname, presence: true
+  # validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+  # validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' }
+  # validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字、ひらがな、全角カタカナのみ使用できます' }
+  # validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' }
+  # validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用できます' }
+  # validates :birthday, presence: true
+
 end

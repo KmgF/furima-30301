383a312c106345d333bfc33f400a55c5744ca39d
KmgF <72263892+KmgF@users.noreply.github.com>
2020-11-27 10:03:52 +0900

    priceカラムのバリデーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,12 @@ class Product < ApplicationRecord
     validates :user_id
   end
 
+  validates :price  format: {with: /\A[0-9]+\z/,message:'半角数字のみ使用できます'},
+                    numericality:{greater_than_or_equal_to: 300 ,message:'値段は最低300円以上に設定してください。'},
+                    numericality:{less_than_or_equal_to: 9999999 ,message:'9999999円以上は設定できません'}
+    
+
+
   belongs_to :user
 
 end

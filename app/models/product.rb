dc682654057f679f483fff4831ec6661f090ca81
KmgF <72263892+KmgF@users.noreply.github.com>
2020-11-27 09:44:30 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  with_options presence: true do
+    validates :name 
+    validates :description 
+    validates :price
+    validates :category_id 
+    validates :status_id 
+    validates :shipment_prefecture_id
+    validates :date_of_shipment_id
+    validates :delivery_fee_id
+    validates :user_id
+  end
+
 end

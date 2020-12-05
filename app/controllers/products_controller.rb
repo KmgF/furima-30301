039ab66f3733863beb7c5cf1a8b2484c71c2adbe
KmgF <72263892+KmgF@users.noreply.github.com>
2020-12-05 17:26:22 +0900

    投稿成功時、失敗時の画面遷移の設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,11 +6,18 @@ class ProductsController < ApplicationController
   @product = Product.new
   end
 
-  def create(product_params)
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def product_params
     params.require(:product).permit(:name,:description,:category_id,:status_id,:delivery_fee_id,:shipment_prefecture_id,:date_of_shipment_id,:price,:image).merge(user_id: current_user.id)
   end
+
 end

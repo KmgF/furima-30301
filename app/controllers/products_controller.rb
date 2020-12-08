cf348e33a2d8cb8ce7a6377bb7ed6987ead9c0b0
KmgF <72263892+KmgF@users.noreply.github.com>
2020-12-08 21:46:51 +0900

    Productコントローラー　誤字訂正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, expect: [:index]
+  before_action :authenticate_user!, except: :index
+  
   def index
   end
 

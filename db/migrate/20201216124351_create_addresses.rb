e5cb67338fd5f368dd1a936936eb9cb946199d1d
KmgF <72263892+KmgF@users.noreply.github.com>
2020-12-21 14:31:50 +0900

    Addressテーブルの外部キー設定

--- db/migrate/20201216124351_create_addresses.rb
+++ db/migrate/20201216124351_create_addresses.rb
@@ -7,7 +7,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string :house_number,   null: false
       t.string :building_name,  null: false
       t.string :tel,            null: false
-      t.integer :buyer_id,      null: false
+      t.integer :buyer_id,      null: false, foreign_key: true
       t.timestamps
     end
   end

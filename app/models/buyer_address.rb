ef04259b88a1c4d6d70a0fc188575cdb082e986a
KmgF <72263892+KmgF@users.noreply.github.com>
2020-12-24 21:30:16 +0900

    バリデーションの設定

--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -2,6 +2,17 @@ class BuyerAddress
 
   include ActiveModel::Model
   attr_accessor :user_id,:product_id,:post_number,:prefecture_id,:city,:house_number,:building_name,:tel,:buyer_id
+  attr_accessor :user_id,:product_id,:post_number,:prefecture_id,:city,:house_number,:building_name,:tel,:token
+
+  with_options presence: true do
+    validates :product_id
+    validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message:"ハイフンを入れてください"}
+    validates :prefecture_id, numericality: { other_than: 1, message: '都道府県を選択してください' }
+    validates :city
+    validates :house_number
+    validates :tel, numericality: true,length: {maximum: 11},exclusion: {in: %w(-) ,message:"ハイフンは入力しないでください"}
+    validates :token
+  end
 
   def save
     Buyer.create(user_id: user_id,product_id: product_id)

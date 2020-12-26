49801f7d86224bbe70f2646c7453498729f98399
KmgF <72263892+KmgF@users.noreply.github.com>
2020-12-26 17:33:47 +0900

    ログインアウト状態のユーザーが購入画面に遷移しようとすると、ログイン画面になる

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,4 +1,6 @@
 class BuyersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @product = Product.find(params[:product_id])
     @buyer_address = BuyerAddress.new

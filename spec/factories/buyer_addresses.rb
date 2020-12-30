7aee839466abd0eaa3b083efefe906f87afa4c5a
KmgF <72263892+KmgF@users.noreply.github.com>
2020-12-31 01:17:05 +0900

    不要な記述の削除（buyer_addressのfactorybot）

--- spec/factories/buyer_addresses.rb
+++ spec/factories/buyer_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :buyer_address do
     post_number   { '123-4567' }
-    prefecture_id { '2' }
+    prefecture_id { 2 }
     city          { '札幌市' }
     house_number  { '1-1-1' }
     building_name { '札幌ドーム' }

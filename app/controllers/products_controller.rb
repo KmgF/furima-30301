674b5d1a845012ead117a409e61f26a030b24477
KmgF <72263892+KmgF@users.noreply.github.com>
2020-12-31 18:40:22 +0900

    @product = Product.find(params[:id])をbefore_actionへまとめる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product
 
   def index
     @products = Product.order('created_at DESC')
@@ -19,15 +20,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    set_product
   end
 
   def edit
-    @product = Product.find(params[:id])
+    set_product
   end
 
   def update
-    @product = Product.find(params[:id])
+    set_product
     if @product.update(product_params)
       redirect_to product_path(@product.id)
     else
@@ -39,5 +40,9 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :status_id, :delivery_fee_id, :shipment_prefecture_id, :date_of_shipment_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
